58c9b722ab5541da6545a25c0ad4d862b990d592
Kesha_2024 <den.azarov.2019@mail.ru>
2024-06-10 07:38:52 +0300

    Implemented BasePage and MainPage with a test for opening the main page

---
+++ pages/Main_Page.py
@@ -0,0 +1,8 @@
+from pages.BasePage import BasePage
+
+class MainPage(BasePage):
+    def __init__(self, browser, url):
+        super().__init__(browser, url)
+        # Additional initialization for MainPage if necessary
+
+    # Add methods specific to MainPage
